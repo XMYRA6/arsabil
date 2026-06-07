ac30f3ef09f9afe5ebae6d7455e649f205cbc330
Emre Taner <sylmztaner5@gmail.com>
2026-06-07 23:16:26 +0300

    feat: include image field in profile GET response

--- src/app/api/user/profile/[userId]/route.ts
+++ src/app/api/user/profile/[userId]/route.ts
@@ -18,6 +18,7 @@ export async function GET(
         select: {
             id: true,
             name: true,
+            image: true,
             bio: true,
             linkedin: true,
             website: true,
